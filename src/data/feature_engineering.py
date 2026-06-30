e7e8252e18d1c59d4852333975fbc77fddd29534
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-06-30 17:12:40 +0530

    scripting the deature engineering part

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -1,1 +1,13 @@
-# transforms raw data into ML features
+"""
+Phase 1: Step 2: Feature Engineering
+Transforms raw lap data into ML - ready features for both XGBoost (unscaled)
+and Logistic Regression
+"""
+
+
+def run_feature_engineering(save: bool = True):
+    ...
+
+
+if __name__ == "__main__":
+    run_feature_engineering()
